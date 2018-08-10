675fc70e56d4d2d941a6a74d75450dc5edd37ea0
oliviamaddalone <livmaddalone@gmail.com>
2018-08-10 14:48:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,15 @@ class Dog
     @name 
   end
   
-def name=(new_name)
-  @name=new_name 
+  def name=(new_name)
+    @name=new_name 
+  end
   
   def breed 
     @breed
   end
+  
+  def breed=(new_breed)
+    @breed=new_breed
+  end 
 end 
